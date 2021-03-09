63ca9658805df3121a9f28b9387766c6116b6225
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2021-03-10 00:53:18 +0300

    Change superclass to MetaIde

--- hello-world/cli-and-ide-plugin/ide-plugin/src/main/kotlin/io/arrowkt/example/MyIdeMetaPlugin.kt
+++ hello-world/cli-and-ide-plugin/ide-plugin/src/main/kotlin/io/arrowkt/example/MyIdeMetaPlugin.kt
@@ -1,18 +1,18 @@
 package io.arrowkt.example
 
 import arrow.meta.CliPlugin
-import arrow.meta.ide.IdeMetaPlugin
 import arrow.meta.ide.IdePlugin
+import arrow.meta.ide.MetaIde
 import arrow.meta.ide.phases.IdeContext
 import arrow.meta.phases.CompilerContext
 import io.arrowkt.example.plugins.helloWorld
 import kotlin.contracts.ExperimentalContracts
 
 @ExperimentalContracts
-class MyIdeMetaPlugin : IdeMetaPlugin() {
+class MyIdeMetaPlugin : MetaIde {
   @ExperimentalContracts
   override fun intercept(ctx: CompilerContext): List<CliPlugin> =
-    cliPlugins
+    emptyList()
 
   @ExperimentalContracts
   override fun intercept(ctx: IdeContext): List<IdePlugin> =
